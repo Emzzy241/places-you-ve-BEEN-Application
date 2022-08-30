96851cbc088ff6d96787c05bb363b438d86dd5ef
Emzzy241 <emzzyoluwole@gmail.com>
2022-08-30 09:00:47 +0100

    the test on summary() method has been passed, moving on

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Expected Output: testingPlace.placeName = 'Oklahoma'
 Describe: Submit Method
 Test: It will ensure that after user inputs in a new Place, they can also get a summary of that place
 Code:    
-    let testingPlace = new Place("Oklahoma", 19, "21st of September 2015", "I made 4 new friends", yes);
+    let testingPlace = new Place("Oklahoma", 19, "21st of September 2015", "4", "yes");
 
     testingPlace.summary();
 Expected Output: An entire story on user's experience should be summarized
--- js/scripts.js
+++ js/scripts.js
@@ -15,8 +15,8 @@ function Place(placeName, yourAge, dateYouVisitedPlace, howManyFNewFriendsFromPl
 // First method for Place constructor
 
 Place.prototype.summary = function(){
-    "Hi, You don't need to know my name but I have something to tell you about this amazing place I once visited. The name of the place was "
-     + this.placeName + " I visited the place when I was " + this.yourAge + " and the day I visited was on " + this.dateYouVisitedPlace + ". I made a total number of "
-     + this.howManyFNewFriendsFromPlace + ". Thanks for hearing me out, I know you would want to know if I would love to go to this place again and the answer is " + this.wouldYouLikeToGoAgain;
-      
+    return "Hi, You don't need to know my name but I have something to tell you about this place I once visited. The name of the place was "
+     + this.placeName + ", I visited the place when I was " + this.yourAge + " and the day I visited was on the " + this.dateYouVisitedPlace + ". I made a total number of "
+     + this.howManyFNewFriendsFromPlace + " new friends at this place. Thanks for hearing me out, I know you would want to know if I would love to go to this place again and the answer is " + this.wouldYouLikeToGoAgain.toUpperCase();
+
 }
