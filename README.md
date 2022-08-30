f7edf8561fecd6f5f703b59e3fce1405ea3eabb9
Emzzy241 <emzzyoluwole@gmail.com>
2022-08-30 08:47:47 +0100

    code for a summary() method has been written and a test has also been written

--- README.md
+++ README.md
@@ -10,3 +10,14 @@ Code:
     let testingPlace = new Place("Oklahoma", 19, "21st of September 2015", "I made 4 new friends", "yes");
 
 Expected Output: testingPlace.placeName = 'Oklahoma'
+
+
+<!-- The next test is on a summary button to summarize a user's experience to another user -->
+
+Describe: Submit Method
+Test: It will ensure that after user inputs in a new Place, they can also get a summary of that place
+Code:    
+    let testingPlace = new Place("Oklahoma", 19, "21st of September 2015", "I made 4 new friends", yes);
+
+    testingPlace.summary();
+Expected Output: An entire story on user's experience should be summarized
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// N
+
 // the constructor format for creating new places
 
 function Place(placeName, yourAge, dateYouVisitedPlace, howManyFNewFriendsFromPlace , wouldYouLikeToGoAgain){
@@ -6,4 +8,15 @@ function Place(placeName, yourAge, dateYouVisitedPlace, howManyFNewFriendsFromPl
     this.dateYouVisitedPlace = dateYouVisitedPlace;
     this.howManyFNewFriendsFromPlace = howManyFNewFriendsFromPlace;
     this.wouldYouLikeToGoAgain = wouldYouLikeToGoAgain;
+}
+
+
+// defining a method that will summarize everything for the user
+// First method for Place constructor
+
+Place.prototype.summary = function(){
+    "Hi, You don't need to know my name but I have something to tell you about this amazing place I once visited. The name of the place was "
+     + this.placeName + " I visited the place when I was " + this.yourAge + " and the day I visited was on " + this.dateYouVisitedPlace + ". I made a total number of "
+     + this.howManyFNewFriendsFromPlace + ". Thanks for hearing me out, I know you would want to know if I would love to go to this place again and the answer is " + this.wouldYouLikeToGoAgain;
+      
 }
